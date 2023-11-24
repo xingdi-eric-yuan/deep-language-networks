a4ea6edb85505f120a41fdd6d3c1ad84160973cb
Xingdi (Eric) Yuan <xingdi-eric-yuan@users.noreply.github.com>
2023-11-24 16:22:19 -0500

    validation

--- projects/simple_dln/main.py
+++ projects/simple_dln/main.py
@@ -2,6 +2,11 @@ from argparse import ArgumentParser
 import datetime
 import logging
 import os
+import tqdm
+import re
+import numpy as np
+from collections import Counter
+from termcolor import colored
 
 from dln.dataset import Dataset, init_dataset
 
@@ -12,27 +17,73 @@ from dln.vi.model import log_message
 from layers import DLN_2
 
 
-# try:
-#     from wandb.integration.openai import autolog
-#     autolog({"project":"dwln"})
-# except ImportError:
-#     pass
+def gsm8k_loss(y_hat, y):
+    # y_hat: a list of strings
+    # y: a list of strings
+    assert len(y_hat) == len(y)
+    loss = []
+    for i in range(len(y_hat)):
+        y_numbers = re.findall(r'\b\d+\b', y[i])
+        y_hat_numbers = re.findall(r'\b\d+\b', y[i])
+        if len(set(y_numbers) & set(y_hat_numbers)) > 0:
+            loss.append(0.0)
+        else:
+            loss.append(1.0)
+    return loss
+
+
+def validate(model, dataset: Dataset, iteration):
+
+    log_message("===================================")
+    log_message(colored("VALIDATING... ITER %s" % str(iteration), "red"))
+    log_message("Current L1 weights:", model.l1.prompt, "This layer is " + ("trainable" if model.l1.trainable else "fixed"))
+    log_message("Current L2 weights:", model.l2.prompt, "This layer is " + ("trainable" if model.l2.trainable else "fixed"))
+
+    acc = 0.0
+    tot = 0.0
+    pbar = tqdm.tqdm(
+        total=dataset.dev_size,
+        bar_format="{l_bar}{bar:10}{r_bar}{bar:-10b}",
+        desc="Eval",
+    )
+    dataset.reset_pointer("dev")
+
+    for batch in dataset.iterate("dev", batch_size=20):
+        x, y, _ = batch
+        y_hat = model.forward(x)
+        losses = gsm8k_loss(y_hat, y)
+        acc += len(y) - np.sum(losses)
+        tot += len(y)
+
+        pbar.update(len(y))
+        pbar.set_postfix_str(f"{acc / tot:.1%}")
+    
+    dev_acc = acc / tot
+    if iteration == 0:
+        log_message(colored("INIT DEV ACC: {}".format(dev_acc), "red"))
+    log_message(colored("DEV ACC: {}".format(dev_acc), "red"))
+    return dev_acc
 
 
 def train(model, dataset: Dataset, batch_size, iters):
-    for _ in range(iters):
+
+    _ = validate(model, dataset, 0)
+    for iter_num in range(iters):
         x, y, _ = dataset.get_batch("train", batch_size, random_sample=True)
         y_hat = model.forward(x)
         h, input, = model.h, model.inputs
         model.backward(y)
-        print("===================================")
-        print("------- L1")
-        print(model.l1.prompt)
-        print("------- L2")
-        print(model.l2.prompt)
+        log_message("===================================")
+        log_message(colored("------- L1"), "red")
+        log_message(colored(model.l1.prompt), "red")
+        log_message(colored("------- L2"), "red")
+        log_message(colored(model.l2.prompt), "red")
         for i, (a, b, c, d) in enumerate(zip(input, h, y_hat, y)):
-            print("-------------------------------" + str(i))
-            print(f"--------------\nx: {a}\nh: {b}\ny_hat: {c}\ny: {d}\n")
+            log_message("-------------------------------" + str(i))
+            log_message(f"--------------\nx: {a}\nh: {b}\ny_hat: {c}\ny: {d}\n")
+        
+        import pdb; pdb.set_trace()
+        _ = validate(model, dataset, iter_num + 1)
 
 
 def train_dln(args):
