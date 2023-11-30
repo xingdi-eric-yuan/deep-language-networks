f88ab34fe9002b46426c041600325a5e805d0523
Xingdi (Eric) Yuan <xingdi-eric-yuan@users.noreply.github.com>
2023-11-30 12:49:40 -0500

    scripts

---
+++ projects/simple_dln/scripts/dwln2_summary_gsm8k.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+set -x  # print commands to terminal
+
+dataset="gsm8k"
+dir=log/dwln2/summary/${dataset}
+
+for seed in 13 42 25; do
+    python dwln2.py \
+        --config llm_config.yaml \
+        --fwd_model gpt-3-fwd \
+        --bwd_model gpt-3-bwd \
+        --dataset ${dataset} \
+        --output_scoring_function accuracy \
+        --max_train_size 400 \
+        --batch_size 20 \
+        --iters 50 \
+        --patience 2 \
+        --num_samples 20 \
+        --aggregation summary \
+        --seed ${seed} \
+        --out_dir ${dir}
+done
