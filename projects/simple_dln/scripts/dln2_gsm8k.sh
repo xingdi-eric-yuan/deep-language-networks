a7a63d9fa49df2212cfa700574e8aa6572ed19c9
Xingdi (Eric) Yuan <xingdi-eric-yuan@users.noreply.github.com>
2023-12-12 13:50:59 -0500

    minor

--- projects/simple_dln/scripts/dln2_gsm8k.sh
+++ projects/simple_dln/scripts/dln2_gsm8k.sh
@@ -2,30 +2,34 @@
 set -x  # print commands to terminal
 
 dataset="gsm8k"
-dir=log/dec7/dln2/${dataset}
+dir=log/dec12/dln2/${dataset}
 prompt_backward_template="ln_prompt_backward:1.0"  # "ln_prompt_backward:2.0"
 input_backward_template="ln_input_backward:1.0"  # "ln_input_backward:2.0"
 
-for num_samples in 10 50; do
-    for first_layer_contrastive in False True; do
-        for score_input_phx in False True; do
-            for seed in 13 42 25; do
-                python dln2.py \
-                    --config llm_config.yaml \
-                    --fwd_model gpt-3-fwd \
-                    --bwd_model gpt-3-bwd \
-                    --dataset ${dataset} \
-                    --max_train_size 400 \
-                    --batch_size 10 \
-                    --iters 10 \
-                    --patience 2 \
-                    --num_samples ${num_samples} \
-                    --seed ${seed} \
-                    --out_dir ${dir}/${prompt_backward_template}_${input_backward_template}_contrastive${first_layer_contrastive}_phx${score_input_phx}_sample${num_samples} \
-                    --prompt_backward_template ${prompt_backward_template} \
-                    --input_backward_template ${input_backward_template} \
-                    --first_layer_contrastive ${first_layer_contrastive} \
-                    --score_input_phx ${score_input_phx}
+# for num_samples in 10 50; do
+for num_samples in 10; do
+    for normalize_score in True; do
+        for first_layer_contrastive in False True; do
+            for score_input_phx in False True; do
+                for seed in 13 42 25; do
+                    python dln2.py \
+                        --config llm_config.yaml \
+                        --fwd_model gpt-3-fwd \
+                        --bwd_model gpt-3-bwd \
+                        --dataset ${dataset} \
+                        --max_train_size 400 \
+                        --batch_size 10 \
+                        --iters 10 \
+                        --patience 2 \
+                        --num_samples ${num_samples} \
+                        --seed ${seed} \
+                        --out_dir ${dir}/${prompt_backward_template}_${input_backward_template}_contrastive${first_layer_contrastive}_phx${score_input_phx}_sample${num_samples}_norm${normalize_score} \
+                        --prompt_backward_template ${prompt_backward_template} \
+                        --input_backward_template ${input_backward_template} \
+                        --first_layer_contrastive ${first_layer_contrastive} \
+                        --score_input_phx ${score_input_phx} \
+                        --normalize_score ${normalize_score}
+                done
             done
         done
     done
