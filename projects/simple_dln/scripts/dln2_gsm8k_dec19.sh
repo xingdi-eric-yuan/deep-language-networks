94403367230439057f10345628ae0183f40a1cf5
Xingdi (Eric) Yuan <xingdi-eric-yuan@users.noreply.github.com>
2023-12-19 12:30:48 -0500

    minor

--- projects/simple_dln/scripts/dln2_gsm8k_dec19.sh
+++ projects/simple_dln/scripts/dln2_gsm8k_dec19.sh
@@ -3,7 +3,7 @@ set -x  # print commands to terminal
 
 dataset="gsm8k"
 dir=log/dec19/dln2/${dataset}
-prompt_backward_template="ln_prompt_backward:2.0"  # "ln_prompt_backward:2.0"
+prompt_backward_template="ln_prompt_backward:1.0"  # "ln_prompt_backward:2.0"
 input_backward_template="ln_input_backward:1.0"  # "ln_input_backward:2.0"
 first_layer_contrastive=True
 num_samples=10
